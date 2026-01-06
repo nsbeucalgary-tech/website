ed42981e5aae6c34d48e35c404442bb422183ff0
fisayoadabs <fisayoadabs@gmail.com>
2026-01-06 06:12:32 -0700

    chore: nextjs initial implementation

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+
+export default function RootLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+    return (
+        <html lang="en">
+            <body className="min-h-screen flex flex-col">
+                <Navbar />
+
+                <main className="flex-1">{children}</main>
+
+                <Footer />
+            </body>
+        </html>
+    );
+}
