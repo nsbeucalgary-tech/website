e22d1bbe76870125025c47f54d562f76e9beebb2
fisayoadabs <fisayoadabs@gmail.com>
2026-01-14 00:16:49 -0700

    fix: testing out revalidation on production branch

--- app/team/page.tsx
+++ app/team/page.tsx
@@ -1,6 +1,9 @@
 import { getAllExecs } from "../lib/execCall";
 import TeamClient from "./components/TeamClient";
 
+export const dynamic = "force-dynamic";
+
+
 export default async function TeamPage() {
     const execs = await getAllExecs();
 
