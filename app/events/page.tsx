f33dee3cf99eb4249dc009bcdf2d09aa05ce294a
fisayoadabs <fisayoadabs@gmail.com>
2026-01-14 00:20:47 -0700

    fix: testing out revalidation on production branch using dynamic rendering

--- app/events/page.tsx
+++ app/events/page.tsx
@@ -1,5 +1,6 @@
 import { getAllEvents } from "../lib/eventCall";
 import EventsClient from "./components/EventClient";
+export const dynamic = "force-dynamic";
 
 export default async function EventsPage() {
     const events = await getAllEvents();
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import SponsorsSection from "./components/SponsorsSection";
 import WhatWeOffer from "./components/WhatWeOffer";
 import { imageDatabase, offerItems } from "./lib/helper";
 import { getAllSponsors } from "./lib/sponsorCall";
+export const dynamic = "force-dynamic";
 
 export default async function HomePage() {
     const sponsors = await getAllSponsors();
